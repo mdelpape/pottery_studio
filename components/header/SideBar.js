dc2f76ccba5a78b08f60b5448495f702e7841cc5
Michael Del Pape <m4delpape@gmail.com>
2024-05-22 12:16:23 -0700

    Update

--- components/header/SideBar.js
+++ components/header/SideBar.js
@@ -1,41 +1,48 @@
-'use client'
-import Link from 'next/link';
-import portfolio from '@/public/assets/profile.jpeg'
-import profile from '@/public/assets/BD65E39F-6691-4C78-9843-896CC917640B_1_105_c.jpeg'
-import ProjectCard from '@/components/2d/ProjectCard.js';
-import Vase1 from '@/components/3d/Vase1.js';
-import Vase2 from '@/components/3d/Vase2.js';
-import Vase5 from '@/components/3d/Vase5.js';
-
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import ProjectCard from "@/components/2d/ProjectCard.js";
 
 export default function SideBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
-    <div className="absolute  m-2 flex flex-col gap-3 items-center z-50">
-      {/* <Link href="/raining_pottery">
-        <ProjectCard Element={'Raining Pots'} />
-      </Link> */}
-      <Link href="/upload">
-        <ProjectCard Element={'Upload'} />
-      </Link>
-      <Link href="/chunk">
-        <ProjectCard Element={'Chunk'} />
-      </Link>
-      <Link href="/house">
-        <ProjectCard Element={'Minecraft House'} />
-      </Link>
-      <Link href="/flow_field_animation">
-        <ProjectCard Element={'Flow Field'} />
-      </Link>
-      <Link href="/">
-        <ProjectCard Element={'Earth'} />
-      </Link>
-      <Link href="/city_head">
-        <ProjectCard Element={'City Head'} />
-      </Link>
-      <Link href="/point_cloud">
-        <ProjectCard Element={'Point Cloud'} />
-      </Link>
-    </div>
+      <div className="absolute m-2 flex flex-col items-center z-50 w-32">
+        <button
+          onClick={toggleDropdown}
+          className="bg-gray-800 text-white px-4 py-2 rounded"
+        >
+          Menu
+        </button>
+        {isOpen && (
+          <div className="mt-2 flex flex-col gap-3 bg-gray-800 p-4 rounded shadow-lg">
+            <Link href="/upload">
+              <ProjectCard Element={"Upload"} />
+            </Link>
+            <Link href="/chunk">
+              <ProjectCard Element={"Chunk"} />
+            </Link>
+            <Link href="/house">
+              <ProjectCard Element={"Minecraft House"} />
+            </Link>
+            <Link href="/flow_field_animation">
+              <ProjectCard Element={"Flow Field"} />
+            </Link>
+            <Link href="/">
+              <ProjectCard Element={"Earth"} />
+            </Link>
+            <Link href="/city_head">
+              <ProjectCard Element={"City Head"} />
+            </Link>
+            <Link href="/point_cloud">
+              <ProjectCard Element={"Point Cloud"} />
+            </Link>
+          </div>
+        )}
+      </div>
   );
 }
--- components/pointCloud/Scene.js
+++ components/pointCloud/Scene.js
@@ -1,10 +1,78 @@
 "use client";
 import { Canvas, useFrame } from "@react-three/fiber";
 import React, { useRef, useState, useEffect } from "react";
-import { OrbitControls, Stars, Clouds, Cloud } from "@react-three/drei";
+import { PerspectiveCamera } from "@react-three/drei";
 import ParticlePlane from "@/components/pointCloud/ParticlePlane.js";
+import gsap from "gsap";
+
+const LookAt = React.forwardRef((props, ref) => {
+  useFrame(() => {
+    if (props.cameraRef.current && ref.current) {
+      props.cameraRef.current.lookAt(ref.current.position);
+    }
+  });
+
+  return <mesh ref={ref} position={[0, 0, 0]} visible={false} />;
+});
 
 export default function Scene() {
+  const cameraRef = useRef();
+  const lookAtRef = useRef();
+  const [planet, setPlanet] = useState("");
+
+  const handlePlanetChange = (event) => {
+    setPlanet(event.target.value);
+  };
+
+  useEffect(() => {
+    if (cameraRef.current) {
+      let position, lookAt;
+      switch (planet) {
+        case "1":
+          position = [10, 10, 10];
+          lookAt = [0, 0, 0];
+          console.log("Mercury");
+          break;
+        case "2":
+          position = [-5, 2, -10];
+          lookAt = [0, 0, -20];
+          console.log("Venus");
+          break;
+        case "3":
+          position = [5, 2, -20];
+          lookAt = [10, 0, -30];
+          console.log("Earth");
+          break;
+        default:
+          position = [10, 10, 10];
+          lookAt = [0, 0, 0];
+          break;
+      }
+      // Animate camera position
+      gsap.to(cameraRef.current.position, {
+        x: position[0],
+        y: position[1],
+        z: position[2],
+        duration: 2,
+        ease: "power2.inOut",
+        onUpdate: () => {
+          if (lookAtRef.current) {
+            cameraRef.current.lookAt(...lookAt);
+          }
+        },
+      });
+
+      //animate the lookat position
+      gsap.to(lookAtRef.current.position, {
+        x: lookAt[0],
+        y: lookAt[1],
+        z: lookAt[2],
+        duration: 2,
+        ease: "power2.inOut",
+      });
+    }
+  }, [planet]);
+
   return (
     <>
       <Canvas
@@ -17,24 +85,40 @@ export default function Scene() {
         }}
         shadows
       >
+        <PerspectiveCamera
+          ref={cameraRef}
+          makeDefault
+          position={[10, 10, 10]}
+          fov={75}
+        />
         <mesh position={[0, 0, 0]} castShadow receiveShadow>
-          <sphereGeometry position={[0, 0, 0]} args={[1, 100, 100]} />
-          <meshPhongMaterial
-            color='#416CF5'
-          />
+          <sphereGeometry args={[1, 100, 100]} />
+          <meshPhongMaterial color="#416CF5" emissive="#fff" />
         </mesh>
-        <group
-          position={[0, 0, 0]}
-          rotation={[0, 0, 0]}
-          scale={[1, 1, 1]}
-        >
-        <ParticlePlane />
-
-        </group>
-        <pointLight position={[5, 10, 5]} intensity={400} />
+        <pointLight position={[0, 0, 0]} intensity={400} decay={1.5} />
         <ambientLight intensity={0.05} />
-        <OrbitControls />
+        <mesh position={[0, 0, -20]} castShadow receiveShadow>
+          <sphereGeometry args={[1, 100, 100]} />
+          <meshPhongMaterial color="#416CF5" />
+        </mesh>
+        <mesh position={[10, 0, -30]} castShadow receiveShadow>
+          <sphereGeometry args={[1, 100, 100]} />
+          <meshPhongMaterial color="#416CF5" />
+        </mesh>
+        <LookAt cameraRef={cameraRef} ref={lookAtRef} />
+        {/* <OrbitControls /> */}
       </Canvas>
+      <div className="absolute top-2 right-2 z-50">
+        <select
+          value={planet}
+          onChange={handlePlanetChange}
+          className="p-2 bg-gray-800 text-white rounded"
+        >
+          <option value="1">Sun</option>
+          <option value="2">Mercury</option>
+          <option value="3">Venus</option>
+        </select>
+      </div>
     </>
   );
 }
